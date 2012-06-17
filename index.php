b8cccaa6e0d8f7476d47b0598c9a2543e09c0f61
David Luce <dluce@mail.umw.edu>
2012-06-17 05:22:56 -0400

    spruced up the home page a bit

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 	include ('htmlheader.php');
 ?>
-		<h2> Location Name for current schedule goes here. </h2>
+		<h1> Welcome! </h1>
         	<p>&nbsp;</p>
            	<p>&nbsp;</p>
-       	  <h3>Display of schedules here</h3>
-       	  <p> Tables that display the schedules will go here</p>
+       	  <h3>This Website is for the express use of the Girl Scouts
+			in the Fredericksburg Area, to better schedule booth locations
+			on Spring weekends.</h3>
+       	  <p>The Admin may:
+				<ul>
+					<li>Reset the schedule for any and all locations.</li>
+				</ul>
+		  </p>
+		  <p> Scout Masters may:
+			<ul>
+				<li>Sign up their troop for a time slot. </li>
+				<li>Revoke a time slot their troop is currently signed 
+					up for</li>
+			</ul>
+		  </p>
+		  <p> Everyone can:
+				<ul>
+					<li>View the current schedule for any location by
+						clicking on its corresponding tab to the left.</li>
+				</ul>
+		  </p>
+						
         	<p>&nbsp;</p>
 
 			<p>&nbsp;</p>

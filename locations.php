6f7c16eacc064ce09fbc69500d77aacee3a0e63d
David Luce <dluce@mail.umw.edu>
2012-06-17 02:43:29 -0400

    Made the locatons.php, which displays table output for locations. Got rid of the time6 column in the locations table, there are only 5 timess per day.

--- locations.php
+++ locations.php
@@ -1,11 +1,58 @@
 <?php
 	include ('htmlheader.php');
+	include ('db_connect.php');
 	if(isset($_GET['location'])){
 		$location = $_GET['location'];
-		echo "$location";
+		echo "<h2>Viewing Schedule For Location: $location </h2><br />";
+		$query = "SELECT t.day_of AS day, t.time1, t.time2, t.time3, t.time4, 
+			t.time5
+			FROM times AS t
+			INNER JOIN locations AS l
+			ON l.id = t.location_id
+			WHERE l.name = '$location'";
+		$result = mysql_query($query) or die (mysql_error());
+		
+		//initialize counter and zone for table display
+		$counter = 9;
+		$zone = "AM";
+		
+		//start the table ?>
+		<table>
+		<tr><th>Day</th><th colspan="2">Time</th></tr>
+		<?php
+		while ($row = mysql_fetch_array($result)){
+			//fill the table
+			echo "  <tr><th rowspan=\"5\">". $row['day'] ."</th>";
+			for ($i = 1; $i < 6; $i++){
+				echo " <tr><td>$counter" . ":00" . "$zone</td> ";
+				
+				//utilizes the php array implementation to
+				//iterate through the boolean results
+				if($row[$i]){
+					echo "<td><em>Time already taken</em></td></tr>";
+				}
+				else{
+					echo "<td><a href=\"take_spot.php\">Sign Up</a></td></tr>";
+				}
+				//increment coutner to get the different times
+				$counter += 2;
+				if($counter > 12){
+					$counter = 1;
+					$zone = "PM";
+				}
+			}
+			//reset counter and zone for the sunday table insert
+			$counter = 9;
+			$zone = "AM";
+		}
+		//end the table
+		echo "</table>";
 	}
-?>
-	<h2></h2>
-<?php	
+	else{
+		?>
+		<h2>To get a table of available times for a location, please
+			click on one of the tabs to the left under "Locations"</h2>
+	<?php	
+	}
 	include ('htmlfooter.php');
 ?>

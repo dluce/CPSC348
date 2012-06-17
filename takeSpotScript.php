258d7e0a50897b63906bfd0287d7824de15e8376
Peter M <pmoerman@mail.umw.edu>
2012-06-17 15:26:30 -0400

    add password recovery sort of, and sign up file

--- takeSpotScript.php
+++ takeSpotScript.php
@@ -5,4 +5,42 @@
 	//contains all information for connecting to the database
 	//including error messages
 	
-	
+	if ( empty($_POST['troop']) && empty($_POST['phone']) 
+		&& empty($_POST['name']) && empty($_POST['password'])) {
+		
+		echo "<p> All information is required! </p>"
+		include ('htmlfooter');
+		exit();
+	}
+	else {
+		$name = $_POST['name'];
+		$troop = $_POST['troop'];
+		$phone = $_POST['phone'];
+		$password = $_POST['password'];
+
+		$Sname = $_SESSION['name'];
+		$Stroop = $_SESSION['troop'];
+		$Sphone = $_SESSION['phone'];
+		$Spassword = $_SESSION['pass'];
+
+		$spot = $_GET['spot'];
+		$day = $_GET['day'];
+
+		if ($name==$Sname && $troop==$Stroop && $phone==$Sphone 
+			&& $password==$Spassword) {
+
+			$query = "SELECT * FROM locations WHERE
+				 spot='$spot',day='$day'"
+			$result = mysql_query($query);
+			if ($row= mysql_fetch_array($array)) {
+				$query = "INERT INTO locations
+					 SET troop_id='$troop'
+					 WHERE '$spot'=spot, '$day'=day"
+				$result = mysql_query($query);
+				if ($row = mysql_fetch_array($result) {
+					echo "You have successfully reserved the timeslot.";
+				}
+			}
+		}
+	}
+?>

0b1d8c497d8c298cefb1823daba2eb560ccf1e7a
Peter M <pmoerman@mail.umw.edu>
2012-06-17 17:53:41 -0400

    password stuff

--- lostPassword.php
+++ lostPassword.php
@@ -8,7 +8,7 @@
 	$troop = $_POST['troop'];
 	$user = $_POST['user'];
 	$email = $_POST['email'];
-	$phone = $_POST['phpne'];
+	$phone = $_POST['phone'];
 	$name = $_POST['name'];
 	$pass = $_POST['newpass'];
 	$check = $_POST['checkpass'];
@@ -16,23 +16,13 @@
 	$query = "SELECT * FROM users WHERE username='$user',scout_master_name='$name',phone='$phone',email='$email',troop_number='$troop'";
 	$result = mysql_query($query);
 	if ($row = mysql_fetch_array($result)) {
-<<<<<<< HEAD
 		if ($pass==$check) {
 			$query = "UPDATE users SET password=SHA('$pass') WHERE username='$user',phone='$phone',email='$email'
 				scout_master_name='$name',troop_number='$troop'";
-			$result = mysql_query($query)
-			echo "<p>Password updated successfully.</p>"
+			$result = mysql_query($query);
+			echo "<p>Password updated successfully.</p>";
 			include ('htmlfooter');
 		}
-=======
-		$pass = $row['password'];
-		
-		mail($email,"Cookie Booth Reservation Password - DELETE AFTER READING", "Dear ". $user .", <br />
-			If you did not request this e-mail, please delete it immediately. If you requested your
-			password because you forgot it, please remember it, and delete this e-mail. Your password is below:
-			<br /> <br />
-			Password: " . $pass . " <br /><br /><br /><br /><br />
->>>>>>> bf4798d44edd88f3af80bccc517acc661aa991cf
 
 	} else {
 		echo "We did not find anyone registered with that Username and email.<br /><br /> Please try again!";

31434030ae80d791a1a84f0d6bd792bcbc36fb72
David Luce <lightsaber209@gmail.com>
2012-06-18 04:07:05 -0400

    Removed superfluous files

--- mydbinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$dbhost = 'localhost';
-	$dbuser = 'dluce';
-	$dbpass = 'gurren5';
-	$dbname = 'S12-CPSC348_dluce';
-?>
--- sql_tests.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO times (location_id)
-			SELECT locations.id
-			FROM (SELECT id FROM locations WHERE name = '$new_loc') locations
-			WHERE 1=1;

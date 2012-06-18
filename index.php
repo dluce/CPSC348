fd50e66c180c87b591f70d72e3671a39643d3b62
David Luce <dluce@mail.umw.edu>
2012-06-18 06:54:05 -0400

    still working on html validation

--- index.php
+++ index.php
@@ -3,32 +3,25 @@
 ?>
 		<h1> Welcome! </h1>
         	<p>&nbsp;</p>
-           	<p>&nbsp;</p>
        	  <h3>This Website is for the express use of the Girl Scouts
 			in the Fredericksburg Area, to better schedule booth locations
 			on Spring weekends.</h3>
-       	  <p>The Admin may:
-				<ul>
-					<li>Reset the schedule for any and all locations.</li>
-				</ul>
+			
+       	  <p>The Admin may: <br />
+				- Reset the schedule for any and all locations. <br /> <br />
 		  </p>
-		  <p> Scout Masters may:
-			<ul>
-				<li>Sign up their troop for a time slot. </li>
-				<li>Revoke a time slot their troop is currently signed 
-					up for</li>
-			</ul>
+		  
+		  <p> Scout Leaders may: <br />
+				- Sign up their troop for a time slot. <br /> <br />
+				- Revoke a time slot their troop is currently signed <br /> <br />
+					up for.
 		  </p>
-		  <p> Everyone can:
-				<ul>
-					<li>View the current schedule for any location by
-						clicking on its corresponding tab to the left.</li>
-				</ul>
+		  
+		  <p> Everyone can: <br />
+				- View the current schedule for any location by
+						clicking on its corresponding tab to the left. <br /><br />
 		  </p>
-						
-        	<p>&nbsp;</p>
 
-			<p>&nbsp;</p>
 <?php
 	include ('htmlfooter.php');
 ?>

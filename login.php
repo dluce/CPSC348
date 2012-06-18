2022a7b03e2d9e1772c6086eca886625cf686ba9
David Luce <dluce@mail.umw.edu>
2012-06-18 07:04:51 -0400

    login.php validation

--- login.php
+++ login.php
@@ -29,26 +29,30 @@
 			
 			//check to see if it is the admin account
 			if ($_SESSION['username'] == "admin"){
-				echo "<p><h2>Successful Login as Administrator.</h2> <br />
-					<h3> You may now: </h3> <br />
+				echo "<h2>Successful Login as Administrator.</h2>
+					<h3> You may now: </h3>
+					<p>
 					<ul>
 						<li>Sign Up for time slots in cookie booths.</li> <br />
 						<li>Edit your current time slot. </li> <br />
 						<li>Add locations to the database. </li> <br />
 						<li>Reset the necessary fields in the database
 							for schedule clearing</li> <br />
-					</ul> ";
+					</ul> 
+					</p>";
 				include ('htmlfooter.php');
 				exit();
 			}
 			else {
-				echo "<p><h2>Successful Login.</h2> <br />
+				echo "<h2>Successful Login.</h2> <br />
 					<h3> You may now: </h3> <br />
+					<p>
 					<ul>
 						<li>Sign Up for time slots in cookie booths.</li> <br />
 						<li>Edit your current time slot. </li> <br />
 						<li>Add locations to the database. </li> <br />
-					</ul> ";
+					</ul> 
+					</p>";
 				include ('htmlfooter.php');
 				exit();
 			}
@@ -56,9 +60,10 @@
 		else
 		{
 			include ('htmlheader.php');
-			echo "<p><h2>Login Failed.</h2> <br />
-				Make sure you entered your username and password correctly, <br />
-				or sign up <a href=\"register.php\" >here.</a> ";
+			echo "<h2>Login Failed.</h2> <br />
+				<p>Make sure you entered your username and password correctly, <br />
+				or sign up <a href=\"register.php\" >here.</a> 
+				</p>";
 			include ('htmlfooter.php');
 		}
 	}
@@ -67,16 +72,18 @@
 	?>	
 		<h2>Login</h2>
 		<form method="post" action="login.php">
+		</p>
 			Enter Username: <input type="text" id="username" name="username" /><br /><br />
 			Password: <input type="password" id="password" name="password" /><br /><br />
 			<input type="submit" value="Login" name="submit" />
+		</p>
 		</form>
 		<br />
 		<br />
 		<br />
 		<p> Click the button below to reset your password in case you forgot it. <br /><br />
-			<a href="lostPassForm.php"><img id = "small" src="http://www.wsalometutoring.com/images/forget_img.jpg" alt="Recover your password">
-				</img></a>
+			<a href="lostPassForm.php"><img id = "small" src="http://www.wsalometutoring.com/images/forget_img.jpg" 
+				alt="Recover your password" /></a>
 		</p>		
 	<?php
 	}
